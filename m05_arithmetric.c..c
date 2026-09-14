498882cc830b9a78cd7efbb2d75e6090fde06048
abhivarshith-19 <abhichokka68@gmail.com>
2026-09-15 00:33:34 +0530

    Add files via upload

---
+++ m05_arithmetric.c..c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    
+    printf("enter two number: ");
+    scanf("%d %d", &a, &b);
+    
+    printf("addition = %d\n", a + b);
+    printf("subtraction = %d\n", a - b);
+    printf("multiplication = %d\n", a * b);
+    
+    return 0;
+}
